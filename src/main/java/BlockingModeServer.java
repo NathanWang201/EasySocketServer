2e74b10e5e30b56f5b19b02c1b9155d2761cf539
NathanWang <nathanwang201@hotmail.com>
2015-01-08 16:41:09 +0800

    add charset spec to server;bug fixed

--- src/main/java/BlockingModeServer.java
+++ src/main/java/BlockingModeServer.java
@@ -9,29 +9,29 @@ import java.util.concurrent.Executors;
  * EasyServer is a class that controls the thread pool and all ports.For a single port,the inner class server will perform its function.
  * Created by Nathan on 2015/1/7.
  */
-public class EasyServer {
+public class BlockingModeServer {
 
 	private int threadsPoolSize;//number of threads distributed to each CPU core
-	private static EasyServer easyServer = null;
+	private static BlockingModeServer blockingModeServer = null;
 	private ExecutorService executorService; //thread pool
 	private Map<String,Server> portMap = new HashMap<String, Server>();//stores registered ports and corresponding server instances
 
-	private EasyServer(){
+	private BlockingModeServer(){
 		//Assure default constructor cannot be accessed publicly
 	}
 
 	public static synchronized void initialize(){
-		if(easyServer == null){
-			easyServer = new EasyServer();
+		if(blockingModeServer == null){
+			blockingModeServer = new BlockingModeServer();
 		}
 	}
 
 	//singleton
-	public static EasyServer getInstance(){
-		if(easyServer == null){
+	public static BlockingModeServer getInstance(){
+		if(blockingModeServer == null){
 			initialize();
 		}
-		return easyServer;
+		return blockingModeServer;
 	}
 
 	/**
@@ -41,7 +41,7 @@ public class EasyServer {
 	 * @return
 	 * @throws Exception
 	 */
-	public EasyServer setUpPool(int fixedNum,int threadsPerCore) throws Exception {
+	public BlockingModeServer setUpPool(int fixedNum,int threadsPerCore) throws Exception {
 		if(fixedNum > 0){
 			executorService = Executors.newFixedThreadPool(fixedNum);
 			threadsPoolSize = fixedNum;
@@ -51,7 +51,7 @@ public class EasyServer {
 		}else{
 			throw new Exception("Wrong thread number!");
 		}
-		return easyServer;
+		return blockingModeServer;
 	}
 
 	/**
@@ -61,7 +61,7 @@ public class EasyServer {
 	 * @return
 	 * @throws Exception
 	 */
-	public EasyServer addPort(int port,Handler handler) throws Exception {
+	public BlockingModeServer addPort(int port,Handler handler) throws Exception {
 		Server server = new Server(port);
 		server.setHandler(handler);
 		if(portMap.containsKey(port+"")){
@@ -69,7 +69,7 @@ public class EasyServer {
 		}else{
 			portMap.put(port+"",server);
 		}
-		return easyServer;
+		return blockingModeServer;
 	}
 
 	/**
@@ -78,14 +78,14 @@ public class EasyServer {
 	 * @return
 	 * @throws Exception
 	 */
-	public EasyServer removePort(int port) throws Exception {
+	public BlockingModeServer removePort(int port) throws Exception {
 		Server server = portMap.get(port+"");
 		if(server == null){
 			portMap.remove(port+"");
 		}else{
 			throw new Exception("Port<" + port + "> in use!");
 		}
-		return easyServer;
+		return blockingModeServer;
 	}
 
 	/**
@@ -93,7 +93,7 @@ public class EasyServer {
 	 * @return
 	 * @throws Exception
 	 */
-	public EasyServer startAll() throws Exception {
+	public BlockingModeServer startAll() throws Exception {
 		Iterator iterator = portMap.entrySet().iterator();
 		while(iterator.hasNext()){
 			Map.Entry entry = (Map.Entry)iterator.next();
@@ -101,7 +101,7 @@ public class EasyServer {
 			Server server = (Server)entry.getValue();
 			server.start();
 		}
-		return easyServer;
+		return blockingModeServer;
 	}
 
 	/**
@@ -110,12 +110,12 @@ public class EasyServer {
 	 * @return
 	 * @throws Exception
 	 */
-	public EasyServer start(String port) throws Exception {
+	public BlockingModeServer start(String port) throws Exception {
 		Server server = portMap.get(port+"");
 		if(server.getServerSocket().isClosed()){
 			server.start();
 		}
-		return easyServer;
+		return blockingModeServer;
 	}
 
 	/**
